e8ac01f6a27cd66702ba22f912b32ffb82c6c0aa
Denis Yuzhelevsky <denislevsky@gmail.com>
2021-07-24 22:29:26 +0300

    add youtube video to ws app

--- client-server-sockets/server.js
+++ client-server-sockets/server.js
@@ -1,16 +1,23 @@
-'use strict';
+"use strict";
 
-const WebSocketServer = require('ws').Server
+const WebSocketServer = require("ws").Server;
 const wss = new WebSocketServer({ port: 8081 });
 
-wss.on('connection', ((ws) => {
-  ws.on('message', (message) => {
+const data = Array.from(Array(100000).keys());
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
     console.log(`received: ${message}`);
   });
 
-  ws.on('end', () => {
-    console.log('Connection ended...');
+  ws.on("end", () => {
+    console.log("Connection ended...");
   });
 
-  ws.send('Hello Client');
-}));
+  ws.send("Hello Client");
+
+  setInterval(() => {
+    ws.send(data.toString());
+    console.log(`server sent a message via ws`);
+  }, 1000);
+});
